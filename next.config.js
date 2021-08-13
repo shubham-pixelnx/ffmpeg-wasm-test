d01be456994de7898d2c760fef627bf244015435
Shubham Prajapat <shubham.prajapat@pixelnx.com>
2021-08-13 16:33:47 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	async headers() {
+		console.log("header");
+		return [
+			{
+				source: "/",
+				headers: [
+					{
+						key: "Cross-Origin-Opener-Policy",
+						value: "same-origin",
+					},
+					{
+						key: "Cross-Origin-Embedder-Policy",
+						value: "require-corp",
+					},
+				],
+			},
+		];
+	},
+};
